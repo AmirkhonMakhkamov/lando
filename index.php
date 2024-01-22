5a329ffc25a4410cff6bb68ec3085fba4ed60940
AmirkhonMakhkamov <makhkamov@arizona.edu>
2024-01-21 21:06:48 -0700

    .

--- index.php
+++ index.php
@@ -1,29 +1,3 @@
 <?php
-require('vendor/autoload.php');
-
-use Lando\App\Configs\Config;
-use Lando\App\Controllers\Home\HomeController;
-
-$config = new Config();
-$conn = $config->conn();
-
-$homeController = new HomeController($conn);
-$request = $homeController->get_request();
-
-$googleAuthLink = $homeController->googleAuth->get_link();
-// $appleAuth = new AppleAuth($conn);
-
-switch ($request) {
-	case "dashboard":
-		header('location: dashboard');
-		exit;
-		break;
-
-    case "~new/preview/":
-    	include 'src/Views/Home/new/index.php';
-    	break;
-
-    default:
-    	include 'src/Views/Home/index.php';
-    	break;
-}
+$path = __DIR__;
+echo $path; // Outputs: /Applications/XAMPP/xamppfiles/htdocs/lando3
